22a6dc035f37d7640fff5af57eea4af2b987cc7e
guillermoguevara887 <guillermoguevara887@gmail.com>
2024-03-07 23:40:35 -0800

    add javaScript fuctions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-console.log('Hello, world! ' + API_KEY);
+
+async function getMovies() {
+    const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
+    const data = await res.json();
+    const movies = data.results;
+    console.log(movies);
+    return movies;
+}
+
+
+async function rederMovies() {
+    const movieTrending = await getMovies();
+    let movieRender = "";
+    movieTrending.forEach(element => {
+        movieRender += `
+        <div class= 'movie-container'>
+            
+            <img src= 'https://image.tmdb.org/t/p/w200${element.poster_path}' class="movie-poster">
+            
+
+        </div>
+        
+        `
+    });
+
+    document.getElementById('movie-card').innerHTML += movieRender;
+
+}
+
+async function rederCategory() {
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const data = await res.json();
+    const genre = data.genres;
+    let cateRender = "";
+    genre.forEach(element => {
+        if (element.name != 'War' && element.name != 'History' && element.name != 'Documentary' && element.name != 'Music' && element.name != 'Western' && element.name != 'Mystery' && element.name != 'TV Movie' && element.name != 'Family' && element.name != 'Fantasy' && element.name != 'Animation' && element.name != 'Adventure' && element.name != 'Crime') {
+            cateRender += `
+            <div class= 'genre-container'>
+                <h3>${element.name}</h3>
+            </div>
+        
+        `
+        }
+    });
+
+    document.getElementById('genre-card').innerHTML += cateRender;
+
+}
+rederMovies();
+rederCategory();
+
+
+
+
+
+
+
